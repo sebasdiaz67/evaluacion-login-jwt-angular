18af02824136160415a839a57896839122091c08
Byron Sebastian Rosero Diaz <sebasdiaz67@gmail.com>
2021-04-16 21:37:30 -0500

    creacion proyecto

---
+++ src/main/java/com/login/jwt/angular/LoginJwtAngularApplication.java
@@ -0,0 +1,13 @@
+package com.login.jwt.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginJwtAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginJwtAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login/jwt/angular/LoginJwtAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.jwt.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginJwtAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
