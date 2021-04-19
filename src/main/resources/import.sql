88436f941355fd6605d530e8617a168353534616
Byron Sebastian Rosero Diaz <sebasdiaz67@gmail.com>
2021-04-19 08:37:42 -0500

    credencia usuario

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.usuarios(
+	id, ciudad, contrasenia, edad, identificacion, nombre, nombre_usuario)
+	VALUES ('abcd-1234-gfgf', 'quito', '$2a$10$duIrQnDXKBBIDp4U6KzEmu7qyaXjzTJRAtIFtIOZka.4KTkCWk2mi', 31, '1002003001', 'sebas diaz', 'sebas');
+--contrasenia 12345
